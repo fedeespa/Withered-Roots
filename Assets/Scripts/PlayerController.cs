e223c0e13d378cd8851c1aa1196216c61bc94a50
MartiKusanagi <pachihotwheels@gmail.com>
2026-09-17 21:07:27 -0300

    Scene Changed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,7 +8,7 @@ using UnityEngine.SceneManagement;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
-    public float jumpHeight = 2f;
+    public float jumpHeight = 1f;
 
     private CharacterController controller;
     private Vector2 input;
